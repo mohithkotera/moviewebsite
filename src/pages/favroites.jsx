6d5757803e94a68f31d0db2a046b4529889e7a61
mohithkotera01 <mohithkotera@gmail.com>
2024-04-01 23:16:59 +0530

    some fixes

--- src/pages/favroites.jsx
+++ src/pages/favroites.jsx
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 import { IoMdHeart } from "react-icons/io";
 import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
 
 const Favroites = () => {
   const [favmovie, setFavmovie] = useState([]);
+  const navigate = useNavigate();
+
+   const NavigateTo = (id) => {
+     navigate(`/movie/${id}`);
+   };
 
   useEffect(() => {
     let data = JSON.parse(localStorage.getItem("favorites"));
@@ -21,7 +27,11 @@ const Favroites = () => {
       </div>
       <div className="grid grid-cols-5 gap-6">
         {favmovie?.map((item, i) => (
-          <div className="col-span-1 flex justify-center" key={i}>
+          <div
+            className="col-span-1 flex justify-center"
+            key={i}
+            onClick={() => NavigateTo(item.original_title)}
+          >
             <MovieCard data={item} icon={<IoMdHeart color="red" size={22} />} />
           </div>
         ))}
