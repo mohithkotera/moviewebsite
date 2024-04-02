262859d3391fab182193cf3e49586c7f2bd93746
mohithkotera01 <mohithkotera@gmail.com>
2024-04-02 12:45:14 +0530

    some fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation,useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate()
   const location = useLocation();
 
   return (
     <div className="w-full  px-6 py-4">
       <div className="flex w-full flex-row items-center justify-between">
-        <h1 className="text-[32px] font-bold text-red-500">Movies</h1>
+        <h1 className="text-[32px] font-bold text-red-500 cursor-pointer" onClick={()=>navigate('/')}>Movies</h1>
         <div className="flex gap-x-7">
           <Link
             to="/"
